a8b2ca069c982b3e2d4bf6545f98d58036123d9f
llVeraVerall <Sladkovavera7@gmail.com>
2021-11-12 01:59:17 +0300

    create page Home

--- src/NavBar/Nav.js
+++ src/NavBar/Nav.js
@@ -1,5 +1,8 @@
 import React from "react";
+import {Routes, Route, Link } from 'react-router-dom'
 import './Nav.css'
+import '../media.css'
+import Home from "../Home/Home";
 
 const Nav = () => {
     return (
@@ -11,11 +14,14 @@ const Nav = () => {
                     <span></span>
                     <span></span>
                     <ul id="menu">
-                        <a href="#"><li>Home</li></a>
+                        <Link to="/home"><li>Home</li></Link>
                         <a href="#"><li>Tasks</li></a>
                         <a href="#"><li>Info</li></a>
                         <a href="#"><li>Commits</li></a>
                     </ul>
+                    <Routes>
+                        <Route path='/home' element={<Home />} />
+                    </Routes>
                 </div>
             </nav>
         </div>
