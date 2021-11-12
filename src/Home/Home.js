bfd9ff08e6956917cba7a9cdf9070e275fa04fed
llVeraVerall <Sladkovavera7@gmail.com>
2021-11-12 11:25:17 +0300

    gitirnore

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,26 @@
 import React from "react";
+import './Home.css'
+import '../media.css'
 
 const Home = () => {
     return(
-       <div>
-           kjh
+    <div>
+       <div className='title'>
+           Home
        </div>
+            <div className='block'>
+                <div className='days'><h4>Days</h4>
+                <div className='icon'>img</div>
+                    <div className='limit'>limit</div>
+                    <div className='count'>25</div>
+                </div>
+                <div className='tasks'><h4>Tasks</h4>
+                    <div className='icon'>img</div>
+                    <div className='limit'>limit</div>
+                    <div className='count'>70</div>
+                </div>
+            </div>
+    </div>
     )
 }
 
--- src/NavBar/Nav.js
+++ src/NavBar/Nav.js
@@ -3,27 +3,32 @@ import {Routes, Route, Link } from 'react-router-dom'
 import './Nav.css'
 import '../media.css'
 import Home from "../Home/Home";
+import Tasks from "../Tasks/Tasks";
 
 const Nav = () => {
     return (
         <div>
-            <nav role="navigation">
+            <Home/>
+            {/*<nav role="navigation">
                 <div id="menuToggle">
                     <input type="checkbox" />
                     <span></span>
                     <span></span>
                     <span></span>
+
                     <ul id="menu">
-                        <Link to="/home"><li>Home</li></Link>
-                        <a href="#"><li>Tasks</li></a>
+                        <Link to="/home">Home</Link>
+                        <Link to="#"><li>Tasks</li></Link>
                         <a href="#"><li>Info</li></a>
                         <a href="#"><li>Commits</li></a>
                     </ul>
                     <Routes>
                         <Route path='/home' element={<Home />} />
+                        <Route path='/tasks' element={<Tasks />}/>
                     </Routes>
                 </div>
             </nav>
+            */}
         </div>
     )
 }
